c44809321bf997fc931d424e339cf666a2771e42
dgomex <daniel_ddg@outlook.com>
2019-11-11 00:03:42 +0000

    Set concurrency = 1 and replace bash command by file content

--- airflow/dags/dag_templates/template_dag.py
+++ airflow/dags/dag_templates/template_dag.py
@@ -18,12 +18,12 @@ def create_default_args():
             }
 
 def create_dag(dag_name, args, dag_schedule):
-    return DAG(dag_name, default_args=args, schedule_interval=dag_schedule)
+    return DAG(dag_name, default_args=args, schedule_interval=dag_schedule, concurrency=1)
 
-def create_task(task_name, dag):
+def create_task(task_name, dag, task_script):
     return BashOperator(
             task_id=task_name,
-            bash_command='date',
+            bash_command=task_script,
             dag=dag)
 
 def create_dependencies(task_name, dependencies, tasks):
@@ -33,6 +33,7 @@ def create_dependencies(task_name, dependencies, tasks):
 
 def run_template():
     yaml_path = "/usr/local/airflow/config/"
+    tasks_path = "/usr/local/airflow/tasks/"
     for yaml_file in os.listdir(yaml_path):
         with open(f"{yaml_path}{yaml_file}") as conf_file:
             conf_list = yaml.load(conf_file)
@@ -41,9 +42,11 @@ def run_template():
             with dag:
                 tasks = dict()
                 for task in conf_list["dag_tasks"]:
-                    tasks[task["task_name"]] = create_task(task["task_name"], dag)
-                    if "task_dependencies" in task:
-                        create_dependencies(task["task_name"], task["task_dependencies"], tasks)
+                    with open(f"{tasks_path}{task['path']}", "r") as script:
+                        task_script = script.read()
+                    tasks[task["name"]] = create_task(task["name"], dag, task_script)
+                    if "dependencies" in task:
+                        create_dependencies(task["name"], task["dependencies"], tasks)
                 
                 globals()[uuid.uuid1()] = dag
 
